f6ff76425b8c3a0ba2b369b90879d9880ea04cfd
Seheyon <104759146+Seheyon@users.noreply.github.com>
2023-11-23 10:44:49 +0900

    add split data code

--- main.py
+++ main.py
@@ -1,74 +1,108 @@
-import tensorflow as tf
 import pandas as pd
-from numpy import array
-from keras.models import Sequential
-from keras.layers import Dense, LSTM
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.initializers import Constant
 import matplotlib.pyplot as plt
 
-# CSV 파일에서 데이터 로드
+danigoApp = pd.read_csv("", encoding = 'UTF-8')
 
-data = pd.read_csv('')
+danigoIot1 = pd.read_csv("", encoding = 'UTF-8')
+danigoIot2 = pd.read_csv("", encoding = 'UTF-8')
+danigoIot3 = pd.read_csv("", encoding = 'UTF-8')
+danigoIot4 = pd.read_csv("", encoding = 'UTF-8')
+danigoIot5 = pd.read_csv("", encoding = 'UTF-8')
+
+dataNow = 20230309
+tempAvg = []
+for date in range(5):
+    temp = danigoApp[danigoApp['시작일'] == dataNow]
+
+    temp['시작 시간'] = temp['시작 시간'].str.replace(':', '')
+    temp['시작 시간'] = temp['시작 시간'].astype('int64')
+    temp['시작 시간'] = temp['시작 시간'] * 100
+
+    temp['종료시간'] = temp['종료시간'].str.replace(':', '')
+    temp['종료시간'] = temp['종료시간'].astype('int64')
+    temp['종료시간'] = (temp['종료시간'] * 100) + 59
+
+    start = list(temp['시작 시간'])
+    stop = list(temp['종료시간'])
+
+    for i in range(len(start)):
+        n = danigoIot1[(danigoIot1['등록시간'] <= stop[i]) & (danigoIot1['등록시간'] >= start[i])]
+        tempAvg.append(sum(n['온도(°C)'])/len(n['온도(°C)']))
+        
+    dataNow += 1
+
+danigoApp['Avg_Temperature'] = tempAvg
+    
+#엑셀 저장
+danigoApp.to_excel('danigoAppTem.xlsx', index=False)
 
 # -----ex) 온도와 배터리 소모량 데이터 추출 -----
+# 'temperature' 열에서 온도 데이터 추출
+X = danigoApp['Avg_Temperature'].values
+y = danigoApp['연료 사용량 (km)'].values # 'BatteryConsumption' 열에서 배터리 소모량 데이터 추출
 
-# 'Temperature' 열에서 온도 데이터 추출
-temperature = data['Temperature'].values
-
-battery_consumption = data['BatteryConsumption'].values # 'BatteryConsumption' 열에서 배터리 소모량 데이터 추출
-
-# -----시퀀스 길이와 특성 수를 정의-----
-
-sequence_length = 10
-feature_dim = 1
-
-X_sequence, y_sequence = [], []
-
-# -----입력 시퀀스(X_sequence)와 출력 레이블(y_sequence) 생성-----
-
-for i in range(len(temperature) - sequence_length):
-    X_sequence.append(temperature[i:i + sequence_length])
-    # X_sequence는 온도 데이터의 시퀀스로, 현재 시점에서 이전 시점까지의 데이터 포함
-    y_sequence.append(battery_consumption[i + sequence_length])
-    # y_sequence는 배터리 소모량의 레이블로, 현재 시점에서의 배터리 소모량 포함
-
-X_sequence = np.array(X_sequence)
-# X_sequence를 NumPy 배열로 변환 -> 모델 학습에 사용
-y_sequence = np.array(y_sequence)
-# y_sequence도 NumPy 배열로 변환 -> 모델 학습에 사용
+# 데이터 정규화
+scaler = MinMaxScaler()  # Min-Max 스케일러를 사용하여 데이터를 [0, 1] 범위로 정규화
+X_scaled = scaler.fit_transform(X.reshape(-1, 1))  # 1차원 배열을 열벡터 형태로 변환하여 정규화
 
 # -----데이터 분할 (학습 및 테스트 데이터)-----
 
 # 데이터를 학습 및 테스트 세트로 분할할 비율 설정
-split_ratio = 0.8
-split_index = int(split_ratio * len(X_sequence))
-
-# 데이터를 학습용과 테스트용으로 분할 (X_train과 y_train은 학습데이터, X_test와 y_test는 테스트 데이터)
-X_train, X_test = X_sequence[:split_index], X_sequence[split_index:]
-y_train, y_test = y_sequence[:split_index], y_sequence[split_index:]
+# 학습 및 테스트 데이터로 분할
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)  # 데이터를 학습용과 테스트용으로 나눔
+#(test_size : 데이터를 테스트세트로 사용할 비율)
 
 #-----LSTM 모델 만들기-----
+# 데이터를 LSTM 입력 형태로 변환(데이터 전처리)
+X_train = X_train.reshape((X_train.shape[0], 1, X_train.shape[1]))  # LSTM 입력 형태로 데이터 변환
+#2차원인 X_train을 3차원배열로 바꿔줌
+X_test = X_test.reshape((X_test.shape[0], 1, X_test.shape[1]))
 
-model = Sequential()
-# LSTM 레이어 추가(DENSE와 사용법 동일하나 input_shape=(열, 몇개씩잘라작업))
-model.add(LSTM(10, activation = 'relu', input_shape=(sequence_length, feature_dim)))
- # 출력 레이어 추가(회귀 위해 사용)
-model.add(Dense(1, activation='sigmoid'))
-#모델 컴파일
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+#ex) 가중치 상수 설정
+temperature_weight = 7.0 #2.0
 
-# -----모델 구조 확인-----
+# LSTM 모델 생성
+model = Sequential()  # Sequential 모델 생성
+model.add(LSTM(units=50, activation='relu', input_shape=(X_train.shape[1], X_train.shape[2]), kernel_initializer=Constant(value=temperature_weight)))  # LSTM 레이어 추가
+model.add(Dense(units=1))  # 출력 레이어 추가
 
-model.summary()
+# 모델 컴파일
+model.compile(optimizer='adam', loss='mean_squared_error')
 
-# -----모델 학습-----
+# 모델 학습
+model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_test, y_test))
+#(50번 반복, 한번의 반복동안 32개만큼 사용됨)
 
-model.fit(X_train, y_train, epochs=50, batch_size=32)
+# 모델 평가
+loss = model.evaluate(X_test, y_test)  # 테스트 데이터로 모델을 평가하고 손실 값을 출력(손실값 작을수록 성능 좋음)
+print(f'Mean Squared Error on Test Data: {loss}')
 
-# -----테스트 데이터로 예측-----
-y_pred = model.predict(X_test)
+# 예측
+y_pred = model.predict(X_test)  # 학습된 모델을 사용하여 테스트 데이터에 대한 예측 수행
 
-# -----예측 결과와 실제 데이터 그래프로 그리기-----
-plt.plot(y_test, label='실제 데이터')
-plt.plot(y_pred, label='모델 예측')
+# 시각화
+plt.figure(figsize=(10, 6))
+
+# 실제 데이터와 모델 예측 결과 선 그래프로 표시
+plt.plot(y_test, label='Real Data', marker='o')
+plt.plot(y_pred, label='Prediction', marker='o', linestyle='dashed', color='orange')
+
+# 그래프 제목 및 레이블 설정
+plt.title('Prediction')
+plt.xlabel('temperture')
+plt.ylabel('Consumption')
 plt.legend()
+
+# 추세선 추가
+plt.plot(np.arange(len(y_test)), np.poly1d(np.polyfit(np.arange(len(y_test)), y_test, 1))(np.arange(len(y_test))), label='실제 데이터 추세', linestyle='dashed', color='green')
+plt.plot(np.arange(len(y_test)), np.poly1d(np.polyfit(np.arange(len(y_test)), y_pred.flatten(), 1))(np.arange(len(y_test))), label='모델 예측 추세', linestyle='dashed', color='red')
+
+# 그래프 표시
 plt.show()
