126af0a4cfc02df3a505ea3d27cdc2bd7ddcc619
Jinnyzinny <ddocdoli@jbnu.ac.kr>
2023-12-04 19:31:37 +0900

    main파일에 저장

--- main.py
+++ main.py
@@ -1,124 +1,140 @@
-import pandas as pd
+# 다중회귀 모델을 짜보자
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+import tensorflow as tf
+from matplotlib import pyplot as plt
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
-from tensorflow.keras.initializers import Constant
-import matplotlib.pyplot as plt
 
-danigoApp = pd.read_csv("", encoding = 'UTF-8')
+from sklearn.linear_model import Lasso
 
-danigoIot1 = pd.read_csv("", encoding = 'UTF-8')
-danigoIot2 = pd.read_csv("", encoding = 'UTF-8')
-danigoIot3 = pd.read_csv("", encoding = 'UTF-8')
-danigoIot4 = pd.read_csv("", encoding = 'UTF-8')
-danigoIot5 = pd.read_csv("", encoding = 'UTF-8')
-
-#iot 데이터 합치기
-danigoIotDatas = []
-danigoIotDatas.append(danigoIot1)
-danigoIotDatas.append(danigoIot2)
-danigoIotDatas.append(danigoIot3)
-danigoIotDatas.append(danigoIot4)
-danigoIotDatas.append(danigoIot5)
-
-#시작일 기준 행 추출하기
-dataNow = 20230309
-tempAvg = []
-n = []
-for date in range(5):
-    temp = danigoApp[danigoApp['시작일'] == dataNow]
-
-    temp['시작 시간'] = temp['시작 시간'].str.replace(':', '')
-    temp['시작 시간'] = temp['시작 시간'].astype('int64')
-    temp['시작 시간'] = temp['시작 시간'] * 100
-
-    temp['종료시간'] = temp['종료시간'].str.replace(':', '')
-    temp['종료시간'] = temp['종료시간'].astype('int64')
-    temp['종료시간'] = (temp['종료시간'] * 100) + 59
-
-    start = list(temp['시작 시간'])
-    stop = list(temp['종료시간'])
-
-    for i in range(len(temp)):
-        n.append(danigoIotDatas[date][(danigoIotDatas[date]['등록시간'] <= stop[i]) & (danigoIotDatas[date]['등록시간'] >= start[i])])
-        
-    dataNow += 1
-
-#평균온도계산
-#for i in range(len(n)):
-#    tempAvg.append(sum(n[i]['온도(°C)'])/len(n[i]['온도(°C)']))
+Appdata = pd.read_csv(
+    "/content/gdrive/MyDrive/도로노선정보-20231114/다니고밴-[전주]Type12-역방향/APP/다니고밴.csv"
+)
 
 
-#n[0] = 첫번째 구간 전체 데이터, 인덱스로 접근
+def outlier_remove(data, threshold=2.1):
+    z_scores = np.abs(data - np.mean(data)) / np.std(data)  # Z-score 계산
 
-# danigoApp['Avg_Temperature'] = tempAvg
-    
-#엑셀 저장
-#danigoApp.to_excel('danigoAppTem.xlsx', index=False)
+    filtered_data = data[z_scores < threshold]
+    outlier = data[z_scores > threshold]
 
-# -----ex) 온도와 배터리 소모량 데이터 추출 -----
-# 'temperature' 열에서 온도 데이터 추출
-# X = danigoApp['Avg_Temperature'].values
-# y = danigoApp['연료 사용량 (km)'].values # 'BatteryConsumption' 열에서 배터리 소모량 데이터 추출
+    return filtered_data, outlier
 
-# 데이터 정규화
-# scaler = MinMaxScaler()  # Min-Max 스케일러를 사용하여 데이터를 [0, 1] 범위로 정규화
-# X_scaled = scaler.fit_transform(X.reshape(-1, 1))  # 1차원 배열을 열벡터 형태로 변환하여 정규화
 
-# # -----데이터 분할 (학습 및 테스트 데이터)-----
+Appdata = pd.merge(Appdata, danigoApp)
 
-# # 데이터를 학습 및 테스트 세트로 분할할 비율 설정
-# # 학습 및 테스트 데이터로 분할
-# X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)  # 데이터를 학습용과 테스트용으로 나눔
-# #(test_size : 데이터를 테스트세트로 사용할 비율)
+# X 이상값 추출
+filtered_time, outlier_t = outlier_remove(Appdata["소요 시간(분)"])
+filtered_tl, outlier_tl = outlier_remove(Appdata["신호등 정지"])
+filtered_intemp, outlier_in = outlier_remove(Appdata["In_Temp"])
+filtered_outtemp, outlier_out = outlier_remove(Appdata["Out_Temp"])
+filtered_dist, outlier_dist = outlier_remove(Appdata["거리"])
 
-# #-----LSTM 모델 만들기-----
-# # 데이터를 LSTM 입력 형태로 변환(데이터 전처리)
-# X_train = X_train.reshape((X_train.shape[0], 1, X_train.shape[1]))  # LSTM 입력 형태로 데이터 변환
-# #2차원인 X_train을 3차원배열로 바꿔줌
-# X_test = X_test.reshape((X_test.shape[0], 1, X_test.shape[1]))
+# Y 이상값 추출
+filtered_fuel, outlier_f = outlier_remove(Appdata["연료 사용량 (km)"])
 
-# #ex) 가중치 상수 설정
-# temperature_weight = 7.0 #2.0
+# X 이상값 제거
+for i in outlier_t:
+    Appdata.drop(Appdata[Appdata["소요 시간(분)"] == i].index)
+for i in outlier_tl:
+    Appdata.drop(Appdata[Appdata["신호등 정지"] == i].index)
+for i in outlier_in:
+    Appdata.drop(Appdata[Appdata["In_Temp"] == i].index)
+for i in outlier_out:
+    Appdata.drop(Appdata[Appdata["Out_Temp"] == i].index)
+for i in outlier_dist:
+    Appdata.drop(Appdata[Appdata["거리"] == i].index)
+# Y 이상값 제거
+for i in outlier_f:
+    Appdata = Appdata[Appdata["연료 사용량 (km)"] != i]
 
-# # LSTM 모델 생성
-# model = Sequential()  # Sequential 모델 생성
-# model.add(LSTM(units=50, activation='relu', input_shape=(X_train.shape[1], X_train.shape[2]), kernel_initializer=Constant(value=temperature_weight)))  # LSTM 레이어 추가
-# model.add(Dense(units=1))  # 출력 레이어 추가
+# 값 제거 안하면 0.13441536283220157 [-0.09425034 -0.06246091  0.00426062  0.116113   -0.01573865]
 
-# # 모델 컴파일
-# model.compile(optimizer='adam', loss='mean_squared_error')
+# 값 제거 하면 0.12278575303028283 [-0.08173695 -0.04908362 -0.00548274  0.12570456 -0.01296417]
+Appdata_X = Appdata[["거리", "In_Temp", "Out_Temp", "신호등 정지", "소요 시간(분)"]]
+Appdata_Y = Appdata["연료 사용량 (km)"]
 
-# # 모델 학습
-# model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_test, y_test))
-# #(50번 반복, 한번의 반복동안 32개만큼 사용됨)
+x_train, x_test, y_train, y_test = train_test_split(
+    Appdata_X, Appdata_Y, train_size=0.8, test_size=0.2
+)
 
-# # 모델 평가
-# loss = model.evaluate(X_test, y_test)  # 테스트 데이터로 모델을 평가하고 손실 값을 출력(손실값 작을수록 성능 좋음)
-# print(f'Mean Squared Error on Test Data: {loss}')
+w1 = tf.Variable(tf.random.uniform([1]))
+w2 = tf.Variable(tf.random.uniform([1]))
+w3 = tf.Variable(tf.random.uniform([1]))
+w4 = tf.Variable(tf.random.uniform([1]))
+w5 = tf.Variable(tf.random.uniform([1]))
+b = tf.Variable(tf.random.uniform([1]))
 
-# # 예측
-# y_pred = model.predict(X_test)  # 학습된 모델을 사용하여 테스트 데이터에 대한 예측 수행
 
-# # 시각화
-# plt.figure(figsize=(10, 6))
+def loss_function():
+    # w*x는 행렬 곱셈 후, bias를 더하여 pred_y를 계산
+    pred_y = (
+        w1 * Appdata[["In_Temp"]].values.tolist()
+        + w2 * Appdata[["Out_Temp"]].values.tolist()
+        + w3 * Appdata[["거리"]].values.tolist()
+        + w4 * Appdata[["소요 시간(분)"]].values.tolist()
+        + b
+    )
+    # 평균 제곱근 오차(Mean squared error)를 손실함수(loss function)으로 활용
+    cost = tf.reduce_mean(tf.square(pred_y - Appdata_Y.values.tolist()))
+    return cost
 
-# # 실제 데이터와 모델 예측 결과 선 그래프로 표시
-# plt.plot(y_test, label='Real Data', marker='o')
-# plt.plot(y_pred, label='Prediction', marker='o', linestyle='dashed', color='orange')
 
-# # 그래프 제목 및 레이블 설정
-# plt.title('Prediction')
-# plt.xlabel('temperture')
-# plt.ylabel('Consumption')
-# plt.legend()
+# 보편적으로 가장 많이 사용하는 adam optimizer 활용
+optimizer = tf.optimizers.Adam(learning_rate=0.01)
+# 훈련시키기
+for step in range(3000):
+    cost_val = optimizer.minimize(loss_function, var_list=[w1, w2, w3, b])
+    if step % 100 == 0:
+        # 훈련값 출력
+        print(
+            step,
+            "loss_value:",
+            loss_function().numpy(),
+            "weight:",
+            w1.numpy(),
+            w2.numpy(),
+            w3.numpy(),
+            w4.numpy(),
+            w5.numpy(),
+            "bias:",
+            b.numpy()[0],
+        )
 
-# # 추세선 추가
-# plt.plot(np.arange(len(y_test)), np.poly1d(np.polyfit(np.arange(len(y_test)), y_test, 1))(np.arange(len(y_test))), label='실제 데이터 추세', linestyle='dashed', color='green')
-# plt.plot(np.arange(len(y_test)), np.poly1d(np.polyfit(np.arange(len(y_test)), y_pred.flatten(), 1))(np.arange(len(y_test))), label='모델 예측 추세', linestyle='dashed', color='red')
+pred_y_list = (
+    w1 * Appdata[["In_Temp"]].values.tolist()
+    + w2 * Appdata[["Out_Temp"]].values.tolist()
+    + w3 * Appdata[["거리"]].values.tolist()
+    + w4 * Appdata[["소요 시간(분)"]].values.tolist()
+    + w5 * Appdata[["신호등 정지"]]
+    + b
+)
+print(pred_y_list)  # 실제 y값과 비교
 
-# # 그래프 표시
-# plt.show()
+# 실제 데이터 시각화
+plt.title("Orginal data")
+plt.xlabel("data number")
+plt.ylabel("Label Y value")
+plt.plot(Appdata_X, Appdata_Y, "ro", label="Original data")
+plt.show()
+
+# 예측 데이터 시각화
+plt.title("Predicted value")
+plt.xlabel("data number")
+plt.ylabel("Predicted Y value")
+plt.plot(Appdata_Y, pred_y_list, "bo", label="Predicted data")
+plt.legend()
+plt.show()
+"""
+#General한 예측
+model=LinearRegression()
+model.fit(Appdata_X,Appdata_Y)
+y_predict = model.predict(x_test)
+print(model.score(Appdata_X,Appdata_Y))
+print(model.coef_)
+plt.scatter(y_test, y_predict.astype(int), alpha=0.4)
+plt.xlabel("real Fuel")
+plt.ylabel("predict_fuel_demand")
+plt.title("MULTIPLE LINEAR REGRESSION")
+plt.show() """
